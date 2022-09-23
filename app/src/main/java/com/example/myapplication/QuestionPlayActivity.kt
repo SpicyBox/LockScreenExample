a22b54d92d8eaa965703a4d1eb2c7b8fd4bc00c2
Leejeongchan1733026 <cksqls288@gmail.com>
2022-09-23 23:04:36 +0900

    timer적용

--- app/src/main/java/com/example/myapplication/QuestionPlayActivity.kt
+++ app/src/main/java/com/example/myapplication/QuestionPlayActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class QuestionPlayActivity:AppCompatActivity() {
@@ -8,5 +11,21 @@ class QuestionPlayActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question_play)
+
+        val timerTxt = findViewById<TextView>(R.id.timeTxt)
+        val timeProgressBar = findViewById<ProgressBar>(R.id.timeProgressBar)
+
+        object : CountDownTimer(1000 * 60, 1000) {
+            override fun onTick(p0: Long) {
+                // countDownInterval 마다 호출 (여기선 1000ms)
+                timerTxt.text = (p0 / 1000).toString()
+                timeProgressBar.setProgress(p0.toInt())
+            }
+
+            override fun onFinish() {
+                // 타이머가 종료되면 호출
+            }
+        }.start()
     }
+
 }
