822b2e9be99aa880b3e327bcb0874a8a92ebcd8d
Leejeongchan1733026 <cksqls288@gmail.com>
2022-09-27 23:41:03 +0900

    DB test

--- app/src/main/java/com/example/myapplication/LockActivity.kt
+++ app/src/main/java/com/example/myapplication/LockActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.view.MotionEvent
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class LockActivity : AppCompatActivity(){
@@ -9,15 +11,34 @@ class LockActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lock)
-    }
 
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        return when (event.action) {
-            MotionEvent.ACTION_MOVE -> {
-                finish()
-                true
-            }
-            else -> super.onTouchEvent(event)
+        val multipleChoiceTxt = findViewById<TextView>(R.id.multipleChoiceTxt)
+        val choice1Btn = findViewById<Button>(R.id.choice1Btn)
+        val choice2Btn = findViewById<Button>(R.id.choice2Btn)
+        val choice3Btn = findViewById<Button>(R.id.choice3Btn)
+        val choice4Btn = findViewById<Button>(R.id.choice4Btn)
+
+        multipleChoiceTxt.text = "Cat"
+        choice1Btn.text = "고양이"
+        choice2Btn.text = "개"
+        choice3Btn.text = "나비"
+        choice4Btn.text = "사과"
+
+        choice1Btn.setOnClickListener{
+            Toast.makeText(this, "정답입니다.", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        choice2Btn.setOnClickListener{
+            Toast.makeText(this, "오답입니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        choice3Btn.setOnClickListener{
+            Toast.makeText(this, "오답입니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        choice4Btn.setOnClickListener{
+            Toast.makeText(this, "오답입니다.", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/example/myapplication/QuestionPlayActivity.kt
+++ app/src/main/java/com/example/myapplication/QuestionPlayActivity.kt
@@ -14,11 +14,14 @@ class QuestionPlayActivity:AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question_play)
 
+        val qeustionText = findViewById<TextView>(R.id.qeustionText)
         val timerTxt = findViewById<TextView>(R.id.timeTxt)
         val timeProgressBar = findViewById<ProgressBar>(R.id.timeProgressBar)
         val resultEnterBtn = findViewById<Button>(R.id.resultEnterBtn)
         val answerEnterEdit = findViewById<EditText>(R.id.answerEnterEdit)
         var scoreCount = 0
+
+        qeustionText.text = "Cat"
 
         fun startQuestionPlayActivity(): Unit {
             startActivity(Intent(this,ResultActivity::class.java))
@@ -26,21 +29,26 @@ class QuestionPlayActivity:AppCompatActivity() {
         }
 
         resultEnterBtn.setOnClickListener{
-            scoreCount++
-            Toast.makeText(this, "알림", Toast.LENGTH_SHORT).show()
+            if (answerEnterEdit.text.toString()=="cat"){
+                Toast.makeText(this, "정답입니다.", Toast.LENGTH_SHORT).show()
+                scoreCount++
+            } else {
+                Toast.makeText(this, "오답입니다.", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        object : CountDownTimer(1000 * 6, 1000) {
+        object : CountDownTimer(1000 * 60, 1000) {
             override fun onTick(p0: Long) {
                 // countDownInterval 마다 호출 (여기선 1000ms)
                 timerTxt.text = (p0 / 1000).toString()
                 timeProgressBar.setProgress(p0.toInt())
+
             }
 
             override fun onFinish() {
                 // 타이머가 종료되면 호출
+                startQuestionPlayActivity()
             }
         }.start()
     }
-
 }
