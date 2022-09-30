cce45ee373bd23bbda2191093b4e3770d9f216db
Leejeongchan1733026 <cksqls288@gmail.com>
2022-09-30 19:58:39 +0900

    Room DB test 실패

--- app/src/main/java/com/example/myapplication/QuestionActivity.kt
+++ app/src/main/java/com/example/myapplication/QuestionActivity.kt
@@ -3,9 +3,11 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.room.Room
+import com.example.myapplication.Model.User
 
 class QuestionActivity:AppCompatActivity() {
 
@@ -13,11 +15,18 @@ class QuestionActivity:AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
 
+        lateinit var db: AppDatabase
+
         val englishWordBtn = findViewById<Button>(R.id.englishWordBtn)
         val toeic = findViewById<Button>(R.id.toeicBtn)
 
         englishWordBtn.setOnClickListener{
             startActivity(Intent(this,QuestionPlayActivity::class.java))
         }
+
+        db.userDao().insertHighScore(User("1"))
+        val hightScore = db.userDao().getAll()
+        Toast.makeText(this, hightScore.toString(), Toast.LENGTH_SHORT).show()
     }
+
 }
