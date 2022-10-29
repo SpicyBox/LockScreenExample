8d0a71bbadd63b97890679c4d274f47e8de4b16d
Leejeongchan1733026 <cksqls288@gmail.com>
2022-10-29 21:40:28 +0900

    Realm 정리

--- app/src/main/java/com/example/myapplication/AppDateBase.kt
+++ app/src/main/java/com/example/myapplication/AppDateBase.kt
@@ -2,9 +2,7 @@ package com.example.myapplication
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.myapplication.DAO.QuestionDAO
 import com.example.myapplication.DAO.UserDAO
-import com.example.myapplication.Model.Question
 import com.example.myapplication.Model.User
 
 @Database(entities = [User::class], version = 1)
