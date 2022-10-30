3a50c11e9ee40e61728eaad9aeecca312b8f4517
Leejeongchan1733026 <cksqls288@gmail.com>
2022-10-30 23:16:26 +0900

    firestore 함수 생성

--- app/src/main/java/com/example/myapplication/QuestionPlayActivity.kt
+++ app/src/main/java/com/example/myapplication/QuestionPlayActivity.kt
@@ -50,6 +50,7 @@ class QuestionPlayActivity:AppCompatActivity() {
         var answerList = listOf("cat", "dog", "book", "bus", "phone", "mail", "key", "cap", "chair", "table","cat","time") //정답 리스트(한글 안됨)
         var num = Random().nextInt(9)
         var scoreCount = 0
+        var firestore = 0
 
         qeustionText.text = questionList[num]
 
